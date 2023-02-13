746957a73175a096380ba5d738f190fe332b69e3
김한준 <khjalk8625@kaist.ac.kr>
2023-02-13 16:13:54 +0900

    감정분석 모델 적용

--- eBookBGM/app/src/main/java/org/techtown/ebookbgm/EmotionClassifier.java
+++ eBookBGM/app/src/main/java/org/techtown/ebookbgm/EmotionClassifier.java
@@ -3,19 +3,32 @@ package org.techtown.ebookbgm;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.support.v4.os.IResultReceiver;
+import android.util.Log;
+import android.util.Pair;
 
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EmotionClassifier {
     private static final String MODEL_NAME = "emo_class_keras_model.tflite";
     Context context;
     Interpreter interpreter = null;
+    int modelInputWidth, modelInputHeight, modelInputChannel;
+    int modelOutputClasses;
+    String[] emotion = {"anger", "boredom", "empty", "enthusiasm", "fear", "fun", "happiness",
+            "hate" ,"love", "neutral", "relief", "sadness", "surprise", "worry"};
 
 
     public EmotionClassifier(Context context) {
@@ -26,6 +39,8 @@ public class EmotionClassifier {
         ByteBuffer model = loadModelFile(MODEL_NAME);
         model.order(ByteOrder.nativeOrder());
         interpreter = new Interpreter(model);
+
+        initModelShape();
     }
 
     private ByteBuffer loadModelFile(String modelName) throws IOException{
@@ -37,6 +52,50 @@ public class EmotionClassifier {
         long declaredLength = assetFileDescriptor.getDeclaredLength();
 
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+
+    }
+
+    private void initModelShape(){
+        Tensor inputTensor = interpreter.getInputTensor(0);
+        int[] inputShape = inputTensor.shape();
+
+        for(int i=0;i<inputShape.length;i++){
+            Log.d("MyModel", "Input " + i + ": " + inputShape[i]);
+        }
+        modelInputChannel = inputShape[0];
+        modelInputWidth = inputShape[1];
+
+        Tensor outputTensor = interpreter.getOutputTensor(0);
+        int[] outputShape = outputTensor.shape();
+        for(int i=0;i<outputShape.length;i++){
+            Log.d("MyModel", "Output" + i + ": " + outputShape[i]);
+        }
+        modelOutputClasses = outputShape[1];
+
+    }
+
+    public Map<String, Float> classify(String str){
+        ByteBuffer buffer = convertStringToByteBuffer(str);
+        float [][] result = new float[1][modelOutputClasses];
+        interpreter.run(buffer, result);
+        return argmax(result[0]);
+    }
+
+    private ByteBuffer convertStringToByteBuffer(String str){
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1600);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        byteBuffer.put(str.getBytes(StandardCharsets.UTF_8));
+
+        return byteBuffer;
+    }
+
+    private Map<String, Float> argmax(float[] array){
+
+        Map<String, Float> outputResult = new HashMap<>();
+        for(int i=0;i<emotion.length;i++){
+            outputResult.put(emotion[i], array[i]);
+        }
+        return outputResult;
 
     }
 }
--- eBookBGM/app/src/main/java/org/techtown/ebookbgm/LoadActivity.java
+++ eBookBGM/app/src/main/java/org/techtown/ebookbgm/LoadActivity.java
@@ -3,12 +3,49 @@ package org.techtown.ebookbgm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class LoadActivity extends AppCompatActivity {
+
+    EmotionClassifier emotionClassifier;
+    String str = "It's amazing! said Alice.";
+    String[] emotion = {"anger", "boredom", "empty", "enthusiasm", "fear", "fun", "happiness",
+            "hate" ,"love", "neutral", "relief", "sadness", "surprise", "worry"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        emotionClassifier = new EmotionClassifier(this);
+        try {
+            emotionClassifier.init();
+        } catch (IOException ioe){
+            Log.d("LoadActivity", "failed to init Classifier", ioe);
+        }
+
+        Button button_analysis = findViewById(R.id.button_analysis);
+        TextView textView_analysis = findViewById(R.id.text_analysis);
+
+        button_analysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Float> ans = emotionClassifier.classify(str);
+                String ans_str = "";
+                for(int i=0;i<emotion.length;i++){
+                    ans_str = ans_str + emotion[i] + " : " + ans.get(emotion[i]) + "\n";
+                }
+                textView_analysis.setText(ans_str);
+            }
+        });
+
     }
 }
