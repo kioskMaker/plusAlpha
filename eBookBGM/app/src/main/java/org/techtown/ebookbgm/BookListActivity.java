3c2ef9fa9d80b8e7e9880a81cef2d0c50007dc18
seoyen l <seoyen1122@ewhain.net>
2023-02-24 05:07:45 +0900

    booklist

--- eBookBGM/app/src/main/java/org/techtown/ebookbgm/BookListActivity.java
+++ eBookBGM/app/src/main/java/org/techtown/ebookbgm/BookListActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -18,23 +19,27 @@ public class BookListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_list);
+        setContentView(R.layout.book_item);
         List<BookDataStructure> bookDataStructures = new ArrayList<>();
         AssetManager assetManager = getAssets();
         String[] strings = new String[0];
+
         try {
             strings = assetManager.list("books");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         for(int i=0;i<strings.length;i++){
-            BookDataStructure bookDataStructure = new BookDataStructure(strings[i], getDrawable(R.drawable.book_icon_resize));
+
+            int getId  =getResources().getIdentifier(strings[i], "drawable", this.getPackageName());
+            BookDataStructure bookDataStructure = new BookDataStructure(strings[i], getDrawable(getId));
             bookDataStructures.add(bookDataStructure);
             Log.d("Mybooklist", strings[i]);
         }
 
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
         CustomAdapter customAdapter = new CustomAdapter(bookDataStructures);
         recyclerView.setAdapter(customAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
