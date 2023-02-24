c909a0d13b4a0ccbcfed29fb206261b82b83bd0b
gayoung kim <gaga0916@naver.com>
2023-02-24 14:34:31 +0900

    startactivity 2.5초

--- eBookBGM/app/src/main/java/org/techtown/ebookbgm/InitActivity.java
+++ eBookBGM/app/src/main/java/org/techtown/ebookbgm/InitActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,15 +15,15 @@ public class InitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startactivity);
 
-        Button getStarted = findViewById(R.id.getStarted);
-
-        getStarted.setOnClickListener(new View.OnClickListener() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), BookListActivity.class);
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), BookListActivity.class); //화면 전환
                 startActivity(intent);
+                finish();
             }
-        });
+        }, 2500);
 
     }
 
@@ -30,3 +31,4 @@ public class InitActivity extends AppCompatActivity {
 
 
 }
+
